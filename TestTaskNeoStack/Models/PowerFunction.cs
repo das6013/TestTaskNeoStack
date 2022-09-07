2e699d2063a587c8d612f5ae6ac0e2b7058a3f3a
Das3013 <46927879+das6013@users.noreply.github.com>
2022-09-07 13:55:46 +0700

    Prepare all suo component for work

---
+++ TestTaskNeoStack/Models/PowerFunction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskNeoStack.Models
+{
+    class PowerFunction
+    {
+        /// <summary>Названия Функции.</summary>
+        public string FunctionName { get; }
+    }
+}
