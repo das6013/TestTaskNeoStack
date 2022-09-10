e0a3ab755375ecd247d0747bde2629a95ee3a771
Das3013 <46927879+das6013@users.noreply.github.com>
2022-09-10 10:51:07 +0700

    Create Unit Test

---
+++ TestTaskNeoStackTests1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("TestTaskNeoStackTests1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTaskNeoStackTests1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7e0c0cdc-237d-4806-b5b3-19c88e9cf318")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestTaskNeoStackTests3/Models/PowerFunctionTests.cs
@@ -0,0 +1,112 @@
+﻿using Xunit;
+using TestTaskNeoStack.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTaskNeoStack.Models.Tests
+{
+    public class PowerFunctionTests
+    {
+        [Fact()]
+        public void PowerFunctionConsructorTest()
+        {
+            PowerFunction powerFunctionTest = new PowerFunction("Линейная",
+                new List<double> { 1, 2, 3, 4, 5 },
+                (a, b, c, x, y) => a * x + b * 1 + c);
+            Assert.True(powerFunctionTest!=null, "This test needs an implementation");
+        }
+
+        [Fact()]
+
+        public void MainWindowViewModelTest()
+        {
+            MainWindowViewModel mainWindowViewModelTest = new MainWindowViewModel();
+            Assert.True(mainWindowViewModelTest.Functions.Count() == 5, "This test needs an implementation");
+        }
+
+        [Fact()]
+
+        public void LinearFunctionTest()
+        {
+            MainWindowViewModel mainWindowViewModelTest = new MainWindowViewModel();
+            PowerFunction powerFunctionTest = mainWindowViewModelTest.Functions[0];
+            powerFunctionTest.A = 3;
+            powerFunctionTest.B = 3;
+            powerFunctionTest.C = 3;
+            PowerFunctionRow powerFunctionRowTest = new PowerFunctionRow();
+            powerFunctionRowTest.X = 2;
+            powerFunctionRowTest.Y = 2;
+            powerFunctionRowTest.SetFunction(powerFunctionTest);
+            Assert.True(powerFunctionRowTest.F == 12, "This test needs an implementation");
+        }
+
+        [Fact()]
+
+        public void QuadraticFunctionTest()
+        {
+            MainWindowViewModel mainWindowViewModelTest = new MainWindowViewModel();
+            PowerFunction powerFunctionTest = mainWindowViewModelTest.Functions[1];
+            powerFunctionTest.A = -3;
+            powerFunctionTest.B = 5;
+            powerFunctionTest.C = 10;
+            PowerFunctionRow powerFunctionRowTest = new PowerFunctionRow();
+            powerFunctionRowTest.X = 5;
+            powerFunctionRowTest.Y = 6;
+            powerFunctionRowTest.SetFunction(powerFunctionTest);
+            Assert.True(powerFunctionRowTest.F == -35, "This test needs an implementation");
+        }
+
+        [Fact()]
+
+        public void CubicFunctionTest()
+        {
+            MainWindowViewModel mainWindowViewModelTest = new MainWindowViewModel();
+            PowerFunction powerFunctionTest = mainWindowViewModelTest.Functions[2];
+            powerFunctionTest.A = -2;
+            powerFunctionTest.B = -3;
+            powerFunctionTest.C = 400;
+            PowerFunctionRow powerFunctionRowTest = new PowerFunctionRow();
+            powerFunctionRowTest.X = -8;
+            powerFunctionRowTest.Y = 6;
+            powerFunctionRowTest.SetFunction(powerFunctionTest);
+            Assert.True(powerFunctionRowTest.F == 1316, "This test needs an implementation");
+        }
+
+        [Fact()]
+
+        public void FourthDegreeFunctionTest()
+        {
+            MainWindowViewModel mainWindowViewModelTest = new MainWindowViewModel();
+            PowerFunction powerFunctionTest = mainWindowViewModelTest.Functions[3];
+            powerFunctionTest.A = -5;
+            powerFunctionTest.B = -9;
+            powerFunctionTest.C = 2000;
+            PowerFunctionRow powerFunctionRowTest = new PowerFunctionRow();
+            powerFunctionRowTest.X = -5;
+            powerFunctionRowTest.Y = -9;
+            powerFunctionRowTest.SetFunction(powerFunctionTest);
+            Assert.True(powerFunctionRowTest.F == 5436, "This test needs an implementation");
+        }
+
+        [Fact()]
+
+        public void FifthDegreeFunctionTest()
+        {
+            MainWindowViewModel mainWindowViewModelTest = new MainWindowViewModel();
+            PowerFunction powerFunctionTest = mainWindowViewModelTest.Functions[4];
+            powerFunctionTest.A = 23;
+            powerFunctionTest.B = 78;
+            powerFunctionTest.C = 50000;
+            PowerFunctionRow powerFunctionRowTest = new PowerFunctionRow();
+            powerFunctionRowTest.X = 56;
+            powerFunctionRowTest.Y = 72;
+            powerFunctionRowTest.SetFunction(powerFunctionTest);
+            Assert.True(powerFunctionRowTest.F == 14763041616, "This test needs an implementation");
+        }
+
+
+    }
+}
