0b8ad55b0e760026ed4dd0149bcfe239d9f89b93
Das3013 <46927879+das6013@users.noreply.github.com>
2022-09-14 16:22:45 +0700

    Second fix

--- TestTaskNeoStack/MainWindow.xaml.cs
+++ TestTaskNeoStack/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TestTaskNeoStack
 {
@@ -25,5 +13,13 @@ namespace TestTaskNeoStack
             InitializeComponent();
         }
 
+        private void TextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            int val;
+            if (!Int32.TryParse(e.Text, out val) && e.Text != "-")
+            {
+                e.Handled = true; // отклоняем ввод
+            }
+        }
     }
 }
--- TestTaskNeoStack/Models/PowerFunction.cs
+++ TestTaskNeoStack/Models/PowerFunction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using TestTaskNeoStack.ViewModels;
 using System.Collections.Specialized;
 using System.Collections.ObjectModel;
